a6c328a0030966f7e9e9a5bde338dd4502d0583e
DanielRM <danielrm2899@gmail.com>
2024-11-16 23:51:49 -0600

    update: solution 20

--- SQL/SQL (Basic)/20. Weather Observation Station 5.sql
+++ SQL/SQL (Basic)/20. Weather Observation Station 5.sql
@@ -1,3 +1,12 @@
 --# Problem 20: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
 
 --#Solution
+WITH RankedCities AS (
+    SELECT CITY, LEN(CITY) AS CityLength,
+           RANK() OVER (ORDER BY LEN(CITY), CITY) AS ShortestRank,
+           RANK() OVER (ORDER BY LEN(CITY) DESC, CITY) AS LongestRank
+    FROM STATION
+)
+SELECT CITY, CityLength
+FROM RankedCities
+WHERE ShortestRank = 1 OR LongestRank = 1;
