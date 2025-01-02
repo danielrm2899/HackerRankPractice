1800ae5d3d00d5774c2dd67726a12fbcdcb01238
Daniel <57920323+danielrm2899@users.noreply.github.com>
2025-01-01 23:45:11 -0600

    add: solution 35

---
+++ SQL/SQL (Basic)/35. Top Earners.sql
@@ -0,0 +1,6 @@
+--# Problem 34: https://www.hackerrank.com/challenges/weather-observation-station-16/problem
+
+--#Solution
+SELECT salary*months AS total_earnings, COUNT(*) FROM employee
+GROUP BY 1
+ORDER BY 1 DESC LIMIT 1;
